e2540070000399dd17ac81ff25c758808dd1351e
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2021-07-27 21:13:44 -0300

    Initial Commit

---
+++ src/test/java/com/marcosoft/labone/LabOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosoft.labone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
